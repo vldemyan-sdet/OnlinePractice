13217033ca8c0fa0fe2950d9cebf2a3a206fd625
Volodymyr Demianenko <vldemyan@outlook.com>
2024-11-12 23:10:34 +0200

    remove JsonProperty

--- Evershop.Tests/Evershop.Tests.API/Models/TestRunModel.cs
+++ Evershop.Tests/Evershop.Tests.API/Models/TestRunModel.cs
@@ -4,16 +4,16 @@ namespace Evershop.Tests.API.Models
 {
     public class TestRunModel
     {
-        [JsonProperty("test_name")]
+        //[JsonProperty("test_name")]
         public string TestName { get; set; }
 
-        [JsonProperty("start_time")]
+        //[JsonProperty("start_time")]
         public DateTime StartTime { get; set; }
 
-        [JsonProperty("end_time")]
+        //[JsonProperty("end_time")]
         public DateTime EndTime { get; set; }
 
-        [JsonProperty("result")]
+        //[JsonProperty("result")]
         public string Result { get; set; }
     }
 }
